e537c2c4ef5251e5c092fcd4e50c2e17a4b4a1ed
zerrorTwo <namle022004@gmail.com>
2024-11-13 15:49:56 +0700

    design, handle cart, cart detail

--- src/main/java/com/nghia/coffee_spring_web/config/CustomSuccessHandler.java
+++ src/main/java/com/nghia/coffee_spring_web/config/CustomSuccessHandler.java
@@ -57,8 +57,8 @@ public class CustomSuccessHandler implements AuthenticationSuccessHandler {
             session.setAttribute("avatar", user.getAvatar());
             session.setAttribute("id", user.getId());
             session.setAttribute("email", user.getEmail());
-            // int sum = user.getCart().getSum();
-            // session.setAttribute("sum", sum);
+            int sum = user.getCart() == null ? 0 : user.getCart().getSum();
+            session.setAttribute("sum", sum);
         }
 
     }
--- src/main/java/com/nghia/coffee_spring_web/service/ProductService.java
+++ src/main/java/com/nghia/coffee_spring_web/service/ProductService.java
@@ -5,15 +5,32 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.nghia.coffee_spring_web.domain.Cart;
+import com.nghia.coffee_spring_web.domain.CartDetail;
 import com.nghia.coffee_spring_web.domain.Product;
+import com.nghia.coffee_spring_web.domain.User;
+import com.nghia.coffee_spring_web.repository.CartDetailRepository;
+import com.nghia.coffee_spring_web.repository.CartRepository;
 import com.nghia.coffee_spring_web.repository.ProductRepository;
+
+import jakarta.servlet.http.HttpSession;
 
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+    private final CartRepository cartRepository;
+    private final CartDetailRepository cartDetailRepository;
+    private final UserService userService;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(
+            ProductRepository productRepository,
+            CartRepository cartRepository,
+            CartDetailRepository cartDetailRepository,
+            UserService userService) {
         this.productRepository = productRepository;
+        this.cartRepository = cartRepository;
+        this.cartDetailRepository = cartDetailRepository;
+        this.userService = userService;
     }
 
     public List<Product> findAllProduct() {
@@ -32,4 +49,56 @@ public class ProductService {
         this.productRepository.deleteById(id);
     }
 
+    public void handleAddProductToCart(String email, long productId, HttpSession session) {
+
+        User user = this.userService.getUserByEmail(email);
+        if (user != null) {
+            // check user đã có Cart chưa ? nếu chưa -> tạo mới
+            Cart cart = this.cartRepository.findByUser(user);
+
+            if (cart == null) {
+                // tạo mới cart
+                Cart otherCart = new Cart();
+                otherCart.setUser(user);
+                otherCart.setSum(0);
+
+                cart = this.cartRepository.save(otherCart);
+            }
+
+            // save cart_detail
+            // tìm product by id
+
+            Optional<Product> productOptional = this.productRepository.findById(productId);
+            if (productOptional.isPresent()) {
+                Product realProduct = productOptional.get();
+
+                // check sản phẩm đã từng được thêm vào giỏ hàng trước đây chưa ?
+                CartDetail oldDetail = this.cartDetailRepository.findByCartAndProduct(cart, realProduct);
+                //
+                if (oldDetail == null) {
+                    CartDetail cd = new CartDetail();
+                    cd.setCart(cart);
+                    cd.setProduct(realProduct);
+                    cd.setPrice(realProduct.getPrice());
+                    cd.setQuantity(1);
+                    this.cartDetailRepository.save(cd);
+
+                    // update cart (sum);
+                    int s = cart.getSum() + 1;
+                    cart.setSum(s);
+                    this.cartRepository.save(cart);
+                    session.setAttribute("sum", s);
+                } else {
+                    oldDetail.setQuantity(oldDetail.getQuantity() + 1);
+                    this.cartDetailRepository.save(oldDetail);
+                }
+
+            }
+
+        }
+    }
+
+    public Cart fetchByUser(User user) {
+        return this.cartRepository.findByUser(user);
+    }
 }
