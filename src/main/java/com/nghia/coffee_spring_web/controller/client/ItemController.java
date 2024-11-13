2179edd3214097fec4474c1fd6c4a1f7ef8d6936
zerrorTwo <namle022004@gmail.com>
2024-11-13 12:37:11 +0700

    add cart

--- src/main/java/com/nghia/coffee_spring_web/controller/client/ItemController.java
+++ src/main/java/com/nghia/coffee_spring_web/controller/client/ItemController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import com.nghia.coffee_spring_web.domain.Product;
 import com.nghia.coffee_spring_web.service.ProductService;
 
@@ -24,4 +26,8 @@ public class ItemController {
         return "client/product/detail";
     }
 
+    @PostMapping("/add-product-to-cart/{id}")
+    public String addProductToCart(@PathVariable long id) {
+        return "redirect:/";
+    }
 }
